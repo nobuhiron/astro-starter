908fcfe8f235b04d22c401471760fb4aee3d9442
Nobuhiro Naito <n-naito@kyoeiseicha.co.jp>
2025-12-04 17:54:30 +0900

    修正

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,8 +1,10 @@
 module.exports = {
   plugins: {
     'postcss-import': {},
-    'postcss-custom-media': {},
+    'postcss-custom-media': {
+      importFrom:'src/styles/global/custom_media.css'
+    },
     'postcss-preset-env': { stage: 1 },
-    autoprefixer: {},
+    autoprefixer: { },
   },
 };
